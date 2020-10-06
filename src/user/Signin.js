28372d882d7a97b92e7f5200bb2d22de078600bc
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-06 08:04:16 -0300

    first

---
+++ src/user/Signin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './../core/Layout';
+
+const Signin = () => {
+    return ( 
+    <Layout title="Signin Page" description="Sign to app">
+         ...
+    </Layout>  
+);
+}
+ 
+export default Signin;
---
+++ src/user/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../core/Layout';
+import {API} from '../config';
+
+const Signup = () => {
+    return ( 
+        <Layout title="Signup Page" description="sign up to MERN E-COMMERCE">
+            ...
+        </Layout>
+     );
+}
+ 
+export default Signup;
