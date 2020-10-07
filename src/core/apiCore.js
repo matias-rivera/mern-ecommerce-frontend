e5cecf62414a94387cf7e0bdbc1a10111e8e4935
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-07 12:31:09 -0300

    added display products on home page

---
+++ src/core/apiCore.js
@@ -0,0 +1,14 @@
+import { API } from './../config';
+
+
+//get products by parameters
+export const getProducts = (sortBy) => {
+    return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`,{
+        method: "GET"
+
+    })
+        .then(response => {
+            return response.json()
+        })
+        .catch(err => console.log)
+};
